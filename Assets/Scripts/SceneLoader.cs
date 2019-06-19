2744061c12e8219cb945e5292d52519cc99987cf
Rinita Nair <rnair3@uncc.edu>
2019-06-18 21:44:55 -0400

    Creating the GUI for the game and refactoring the script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        int totalScene = SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene((currentSceneIndex + 1) % totalScene);
+        
+    }
+}
